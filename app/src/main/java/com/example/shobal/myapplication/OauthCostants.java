e3f506c82c43822a5968c2c9133fb37b81f581cf
Brian Kaplan <bakaplan2@gmail.com>
2016-03-31 22:43:20 -0400

    first commit

---
+++ app/src/main/java/com/example/shobal/myapplication/OauthCostants.java
@@ -0,0 +1,8 @@
+package com.example.shobal.myapplication;
+
+public class OauthCostants {
+	final static String CLIENT_ID = "YiXqcG0KkurfFZTS29zwF3Q3bEoa";
+	final static String CLIENT_SECRET = "AnIiTceZw_yE05eYEs9CIgBeUGEa";
+	final static String CALL_BACK_URL = "http://wso2.com";
+	final static String INFO = "INFO";
+}
