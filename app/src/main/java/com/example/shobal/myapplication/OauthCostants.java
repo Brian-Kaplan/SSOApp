f9e995496801206d242e27ba1c90aa05dfc3c051
Brian Kaplan <bakaplan2@gmail.com>
2016-04-08 18:00:23 -0400

    update

--- app/src/main/java/com/example/shobal/myapplication/OauthCostants.java
+++ app/src/main/java/com/example/shobal/myapplication/OauthCostants.java
@@ -1,8 +1,8 @@
 package com.example.shobal.myapplication;
 
 public class OauthCostants {
-	final static String CLIENT_ID = "YiXqcG0KkurfFZTS29zwF3Q3bEoa";
-	final static String CLIENT_SECRET = "AnIiTceZw_yE05eYEs9CIgBeUGEa";
-	final static String CALL_BACK_URL = "http://wso2.com";
+	final static String CLIENT_ID = "WECJfGckiJHyNgd4gOeTymJdTToa";
+	final static String CLIENT_SECRET = "BgDkBgMHrABWysZiSi2z6WyVjkca";
+	final static String CALL_BACK_URL = "http://google.com";
 	final static String INFO = "INFO";
 }
